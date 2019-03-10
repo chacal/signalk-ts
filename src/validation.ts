b4d992eeeaaa0e62e809b17ab7418cd80d5cf460
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-03-10 10:05:06 +0200

    Return validated object instead of the original one
    Only the validated object has default values applied

--- src/validation.ts
+++ src/validation.ts
@@ -2,10 +2,10 @@ import * as Joi from 'joi'
 
 export function parseAndValidate<T>(json: string | T, schema: Joi.SchemaLike): T {
   const jsonObj: T = typeof json === 'string' ? JSON.parse(json) : json
-  const { error } = Joi.validate(jsonObj, schema)
+  const { error, value } = Joi.validate(jsonObj, schema)
   if (error) {
     throw error
   }
-  return jsonObj
+  return value
 }
 
